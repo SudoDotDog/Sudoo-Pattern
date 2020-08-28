7c6757c329ac7cda5d0a6a11d6076cf014364247
WMXPY <wm@mengw.io>
2020-08-28 15:38:45 -0500

    finish schema validation unit tests

--- test/unit/schema.test.ts
+++ test/unit/schema.test.ts
@@ -7,11 +7,10 @@
 
 import { expect } from 'chai';
 import * as Chance from 'chance';
-import { createBigIntPattern, createNumberPattern, createStringPattern, Pattern, validatePatternSchema } from '../../src';
+import { createBigIntPattern, createListPattern, createMapPattern, createNumberPattern, createRecordPattern, createStringPattern, createTuplePattern, Pattern, validatePatternSchema } from '../../src';
 
 describe('Given [Schema] Helper Functions', (): void => {
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const chance: Chance.Chance = new Chance('pattern-schema');
 
     it('should be able to validate string pattern', (): void => {
@@ -31,6 +30,43 @@ describe('Given [Schema] Helper Functions', (): void => {
     it('should be able to validate bigint pattern', (): void => {
 
         const pattern: Pattern = createBigIntPattern();
+
+        expect(validatePatternSchema(pattern)).to.be.true;
+    });
+
+    it('should be able to validate map pattern', (): void => {
+
+        const key: string = chance.string();
+        const pattern: Pattern = createMapPattern({
+            [key]: createStringPattern(),
+        });
+
+        expect(validatePatternSchema(pattern)).to.be.true;
+    });
+
+    it('should be able to validate list pattern', (): void => {
+
+        const pattern: Pattern = createListPattern(createStringPattern());
+
+        expect(validatePatternSchema(pattern)).to.be.true;
+    });
+
+    it('should be able to validate record pattern', (): void => {
+
+        const pattern: Pattern = createRecordPattern(
+            createStringPattern(),
+            createStringPattern(),
+        );
+
+        expect(validatePatternSchema(pattern)).to.be.true;
+    });
+
+    it('should be able to validate tuple pattern', (): void => {
+
+        const pattern: Pattern = createTuplePattern([
+            createStringPattern(),
+            createStringPattern(),
+        ]);
 
         expect(validatePatternSchema(pattern)).to.be.true;
     });
