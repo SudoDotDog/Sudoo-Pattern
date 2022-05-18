e2b111d5dbd6093f188fd85e2d9d3e82bc3f3c94
wmxpy <wm@mengw.io>
2022-05-17 19:35:51 -0500

    version upgrade with common js export

--- src/common.ts
+++ src/common.ts
@@ -8,6 +8,7 @@ export type CommonPattern = {
 
     readonly role?: string;
     readonly optional?: boolean;
+    readonly nullable?: boolean;
     readonly generate?: () => any;
     readonly invalidMessage?: (value?: any) => string;
 };
